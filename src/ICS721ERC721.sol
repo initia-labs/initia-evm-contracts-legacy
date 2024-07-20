f62acfd10be0d7c81031ff987d3a2289e96c5c8d
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2024-07-20 10:35:25 +0700

    feat: initial contracts

---
+++ src/ICS721ERC721.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ICS721ERC721 is ERC721, Ownable {
+    mapping(uint256 => string) private tokenUris;
+    mapping(uint256 => string) private tokenOriginIds;
+    constructor(
+        string memory name_,
+        string memory symbol_
+    ) ERC721(name_, symbol_) Ownable(msg.sender) {}
+
+    function burn(uint256 tokenId) public {
+        address owner = _requireOwned(tokenId);
+        if (!_isAuthorized(owner, msg.sender, tokenId)) {
+            revert ERC721InsufficientApproval(_msgSender(), tokenId);
+        }
+
+        _burn(tokenId);
+    }
+
+    function mint(
+        address receiver,
+        uint256 tokenId,
+        string memory _tokenUri
+    ) public onlyOwner {
+        mint(receiver, tokenId, _tokenUri, "");
+    }
+
+    function mint(
+        address receiver,
+        uint256 tokenId,
+        string memory _tokenUri,
+        string memory _tokenOriginId
+    ) public onlyOwner {
+        _safeMint(receiver, tokenId);
+        tokenUris[tokenId] = _tokenUri;
+        tokenOriginIds[tokenId] = _tokenOriginId;
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        return tokenUris[tokenId];
+    }
+
+    function tokenOriginId(
+        uint256 tokenId
+    ) public view returns (string memory) {
+        return tokenOriginIds[tokenId];
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public override {
+        super.safeTransferFrom(from, to, tokenId, data);
+    }
+}
